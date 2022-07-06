26c8a4a430be0f2665482c6faa981a5cd4026d6c
Mohammed Samaraa Elgohary <51067446+MohammedElgohary@users.noreply.github.com>
2022-07-06 23:30:09 +0200

    feat: adding isOnlone custom hook

---
+++ frmaeworks/react/hooks/useOnline/useOnline.js
@@ -0,0 +1,26 @@
+import { useState, useEffect } from "react";
+
+export const useOnline = () => {
+  const [isOnline, setIsOnline] = useState(window.navigator.onLine);
+
+  useEffect(() => {
+    window.addEventListener("offline", () =>
+      setIsOnline(window.navigator.onLine)
+    );
+
+    window.addEventListener("online", () =>
+      setIsOnline(window.navigator.onLine)
+    );
+
+    return () => {
+      window.removeEventListener("offline", () =>
+        setIsOnline(window.navigator.onLine)
+      );
+      window.removeEventListener("online", () =>
+        setIsOnline(window.navigator.onLine)
+      );
+    };
+  });
+
+  return isOnline;
+};
