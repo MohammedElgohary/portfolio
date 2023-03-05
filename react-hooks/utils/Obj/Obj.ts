f1742df8625e99b67cf5527a465358108b53bdab
MohammedElgohary_Rowaad <m.samara@rowaad.net>
2023-03-05 02:38:18 +0200

    feat: updates

--- react-hooks/utils/Obj/Obj.ts
+++ react-hooks/utils/Obj/Obj.ts
@@ -1,10 +1,20 @@
+type autoComplete<T> = keyof T | Omit<string, keyof T>;
+
 export class Obj {
-  static get(obj: object, key: string, defaultValue: any = null) {
-    return obj[key] || defaultValue;
+  static get<T extends object>(
+    obj: T,
+    key: autoComplete<T>,
+    defaultValue: any = null
+  ) {
+    return obj[key as keyof T] || defaultValue;
   }
 
-  static set(obj: object, key: unknown, value: any = null) {
-    if (value && typeof key === "string") obj[key] = value;
+  static set<T extends object>(
+    obj: T,
+    key: autoComplete<T>,
+    value: any = null
+  ) {
+    if (value && typeof key === "string") obj[key as keyof T] = value;
     else Object.assign(obj, key);
 
     return this;
